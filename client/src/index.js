627b7a8054e61e026d4efabcc1823b762ea57a3d
David Suckstorff <davsucks@umich.edu>
2018-03-15 10:47:10 -0400

    Add nvmrc to client and change how client/App is exported/imported

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './main/index.css';
-import App from './main/App';
+import { App } from './main';
 import registerServiceWorker from './main/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ client/src/main/index.js
@@ -0,0 +1,4 @@
+import App from './App';
+
+// eslint-disable-next-line import/prefer-default-export
+export { App };
