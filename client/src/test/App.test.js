0844d5371030bf257d7a7b3b3b43130a8ebac7e3
David Suckstorff <davsucks@umich.edu>
2018-03-19 08:47:07 -0400

    Write a basic test for app

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from '../main';
+/**
+ * @jest-environment node
+ */
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from 'react';
+import { shallow } from 'enzyme';
+import { App } from '../main/';
+
+fetch.mockResponse(JSON.stringify({ express: 'Hello World' }));
+
+describe('HelloWorld Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders h1', () => {
+    expect(wrapper.find('h1').text()).toEqual('Welcome to React');
+  });
+  it('renders p', () => {
+    expect(wrapper.update().find('p').text()).toEqual('Hello World');
+  });
 });
